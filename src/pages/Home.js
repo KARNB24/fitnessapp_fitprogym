a07c34cb0dc940b8b0dc71fe0a684a306d928cc9
KARNB24 <bhavkarn@sheridancollege.ca>
2024-02-26 17:53:32 -0500

    Done with Navbar and Hero Section

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import { Box } from '@mui/material';
+
+import HeroBanner from '../components/HeroBanner';
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+
+const Home = () => {
+  return (
+    <Box>
+        <HeroBanner />
+        <SearchExercises />
+        <Exercises />
+    </Box>
+  )
+}
+
+export default Home
