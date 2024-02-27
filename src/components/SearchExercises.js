0d5d45a5792b0c0bc27c94ae64c19f5691e54827
KARNB24 <bhavkarn@sheridancollege.ca>
2024-02-27 15:36:40 -0500

    Finished with Home and Exercise Page
    -Fetched Data using RapidAPI's ExerciseDB API and Youtube-Search-And-Download API

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,48 @@
 import React, { useEffect, useState } from "react";
 
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({  setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState("");  
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+
+    fetchExercisesData();
+  }, []);
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
+      
+
+      const searchedExercises = exercisesData.filter(
+        (item) =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
+      );
+      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+
+      setSearch("");
+      setExercises(searchedExercises);
+    }
+  };
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography
@@ -16,6 +56,8 @@ const SearchExercises = () => {
       <Box position="relative" mb="72px">
         <TextField
           height="76px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           sx={{
             input: { fontWeight: "700", border: "none", borderRadius: "4px" },
             width: { lg: "1170px", xs: "350px" },
@@ -37,10 +79,18 @@ const SearchExercises = () => {
             right: "0px",
             fontSize: { lg: "20px", xs: "14px" },
           }}
-          
+          onClick={handleSearch}
         >
           Search
         </Button>
+      </Box>
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyParts
+          setBodyPart={setBodyPart}
+          bodyPart={bodyPart}
+        />
       </Box>
     </Stack>
   );
